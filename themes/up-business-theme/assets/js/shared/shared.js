c796bb1f1df4043a326b3bed8cd17967d7d36544
fidelischimombe <fidelisgomaco@gmail.com>
2023-05-24 20:28:02 -0400

    first commit

---
+++ themes/up-business-theme/assets/js/shared/shared.js
@@ -0,0 +1,15 @@
+import "./_bootstrap";
+
+import "./_header";
+import "./_footer";
+
+import "../sections/hero";
+import "../sections/services";
+import "../sections/brands";
+// import "../sections/portfolio";
+
+// import "../sections/testimonials";
+import "../sections/cta";
+
+import "../sections/blog-list";
+import "../sections/blog-single";
